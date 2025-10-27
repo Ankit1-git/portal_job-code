3b4b85f4cb023ecd31daff9659cd60c50e9351bb
Ankit Sharma <ankitorg15@gmail.com>
2025-10-27 16:16:58 +0530

    domain changes

--- Frontend/src/utils/data.js
+++ Frontend/src/utils/data.js
@@ -1,6 +1,6 @@
-const BASE_URL = "https://portal-job-code-2.onrender.com";
+const BASE_URL = "https://portal-job-code-2.onrender.com/";
 
-export const USER_API_ENDPOINT = `${BASE_URL}/api/user`;
-export const JOB_API_ENDPOINT = `${BASE_URL}/api/job`;
-export const APPLICATION_API_ENDPOINT = `${BASE_URL}/api/application`;
-export const COMPANY_API_ENDPOINT = `${BASE_URL}/api/company`;
+export const USER_API_ENDPOINT = `${BASE_URL}api/user`;
+export const JOB_API_ENDPOINT = `${BASE_URL}api/job`;
+export const APPLICATION_API_ENDPOINT = `${BASE_URL}api/application`;
+export const COMPANY_API_ENDPOINT = `${BASE_URL}api/company`;
